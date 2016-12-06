eaea97789d694f6e16c36cca91026e2db5ac428c
Eugene <e.lavrinenkov@gmail.com>
2016-12-06 19:14:16 +0200

    some changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('#fullpage').fullpage({
-    css3: false,
+    css3: true,
     navigation: true,
     navigationTooltips: ['home', 
                          'intro', 
@@ -12,5 +12,14 @@ $(document).ready(function () {
                          'news',
                          'events',
                          'contact']
+  });
+
+  $.ajax({
+    type: "get",
+    url: "https://www.vi.ai/five-short-videos-on-adtech/",
+    dataType: "jsonp",
+    success: function (response) {
+      console.log();
+    }
   });
 });
