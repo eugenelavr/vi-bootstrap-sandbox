c004470f7528218f808a440caa9cdca686c88ef2
Eugene <e.lavrinenkov@gmail.com>
2016-12-06 15:59:34 +0200

    moved index.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    css3: false,
+    navigation: true,
+    navigationTooltips: ['home', 
+                         'intro', 
+                         'vi', 
+                         'advertisers', 
+                         'publishers', 
+                         'servicelevels', 
+                         'highlight', 
+                         'news',
+                         'events',
+                         'contact']
+  });
 });
