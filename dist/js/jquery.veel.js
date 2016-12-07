7c109258dacc4c7e6e9ff9608dc4adfbdb753a28
Eugene <e.lavrinenkov@gmail.com>
2016-12-07 18:53:39 +0200

    add team wheel dynamic

--- dist/js/jquery.veel.js
+++ dist/js/jquery.veel.js
@@ -1,1 +1,133 @@
-!function(e){"use strict";e.fn.veel=function(t){return this.each(function(){function a(e,t,a){return a<t&&(a+=r),a>e+t}var n=this,s=e.extend({radius:250,startAngle:225,endAngle:270,activeAngle:180},t);e(n).data("opt",s);var i=e("> div",n),r=i.length,o=r/2,l=r-1,g=parseInt(e(this).data("opt").endAngle/l,10),c=parseInt(g/2,10);e.fn.extend({setAngle:function(e,t){var a=-e,n="rotate(#{angle}deg) translateX(#{radius}px) rotate(#{aangle}deg)";n=n.replace(/#{angle}/g,e).replace(/#{aangle}/g,a).replace(/#{radius}/g,t),this.css({transform:n}),this.data("angle",e)},memoNext:function(e){this.data("next-angle",e)},setNext:function(t){this.each(function(){e(this).setAngle(e(this).data("next-angle"),t)})},getAngle:function(){return this.data("angle")}}),i.each(function(t){var a=e(n).data("opt").activeAngle;e(this).hasClass("active")||(a=(t-1)*g+e(n).data("opt").startAngle+c),e(this).setAngle(a,e(n).data("opt").radius)}),i.click(function(t){for(var s=e(this).index(),l=e(".active",n).index(),g=a(o,l,s),c=Math.abs(l-s),d=c<o?c:r-c,u=0;u<r;u++){var h=u<d?r-d+u:u-d;g&&(h=u>=r-d?d-r+u:u+d);var v=e(i[u]).getAngle(),f=e(i[h]).getAngle();g?v>f&&(f+=360):v<f&&(f-=360),e(i[u]).memoNext(f)}i.addClass("animate").removeClass("active"),e(this).addClass("active"),i.setNext(e(n).data("opt").radius),i.trigger("mouseleave"),setTimeout(function(){i.trigger("mouseleave"),i.removeClass("animate")},1500)}),i.mouseenter(function(t){if(!(e("body.mobile").length>0||e(this).hasClass("animate"))){var a=e(this),n=e(".wheel-item-image",a),s=e(".function",a);n.addClass("hover"),a.hasClass("active")||a.mousemove(function(e){var t=a.offset(),i=e.pageX-t.left,r=a.width()/2,o=.6*(i-r);o=o>45?45:o,o=o<-45?-45:o;var l="rotate3d(-0.1, 1, -0.1, {angle}deg) translate3d(0, 0, {z}px)".replace("{angle}",o);n.css({transform:l.replace("{z}","-5"),transition:"none"}),s.css({transform:l.replace("{z}","20"),transition:"none"})})}}),i.mouseleave(function(){e(".wheel-item-image, .function",e(this)).removeClass("hover").removeAttr("style"),e(this).off("mousemove")})})}}(jQuery);
+// vi wheel (veel) jquery plugin
+
+(function($) {
+  'use strict'
+
+  $.fn.veel = function (options) {
+    return this.each(function(){
+      var $container = this;
+      var opt = $.extend({
+        radius: 250,
+        startAngle: 225,
+        endAngle: 270,
+        activeAngle: 180
+      }, options);
+
+      $($container).data('opt', opt);
+
+      var imgs = $('> div', $container),
+          imgLen = imgs.length,
+          imgLenMiddle = imgLen / 2,
+          freeImgLen = imgLen -1,
+          disperceAngle = parseInt($(this).data('opt').endAngle / freeImgLen, 10),
+          halfDisperceAngle = parseInt(disperceAngle / 2, 10),
+          minimumAnimationAngle = 180;
+
+      function countClock(middle, active, clicked) {
+        if (clicked < active) {
+          clicked = clicked + imgLen;
+        }
+        return clicked > (middle + active);
+      }
+
+      $.fn.extend({
+        setAngle: function(angle, radius) {
+          var aAngle = -angle;
+          var transform = 'rotate(#{angle}deg) translateX(#{radius}px) rotate(#{aangle}deg)';
+          transform = transform.replace(/#{angle}/g, angle)
+            .replace(/#{aangle}/g, aAngle)
+            .replace(/#{radius}/g, radius)
+          this.css({
+            'transform' : transform
+          });
+          this.data('angle', angle);
+        },
+        memoNext: function(angle) {
+          this.data('next-angle', angle);
+        },
+        setNext: function(radius) {
+          this.each(function() {
+            $(this).setAngle($(this).data('next-angle'), radius);
+          });
+        },
+        getAngle: function() {
+          return this.data('angle');
+        }
+      });
+
+      imgs.each(function(index) {
+        var angle = $($container).data('opt').activeAngle;
+        if (!$(this).hasClass('active')) {
+          angle = (index - 1) * disperceAngle + $($container).data('opt').startAngle + halfDisperceAngle;
+        }
+        $(this).setAngle(angle, $($container).data('opt').radius);
+      });
+
+      imgs.click(function(evt) {
+        var clickedIndex = $(this).index(),
+            activeInd = $('.active', $container).index(),
+            clockwise = countClock(imgLenMiddle, activeInd, clickedIndex),
+            distance = Math.abs(activeInd - clickedIndex),
+            shortDistance = distance < imgLenMiddle ? distance : imgLen - distance;
+        for(var i = 0; i < imgLen; i++) {
+          var ind = i < shortDistance ? imgLen - shortDistance + i : i - shortDistance;
+          if (clockwise) {
+            ind = i >= (imgLen - shortDistance) ? shortDistance - imgLen + i : i + shortDistance;
+          }
+          var oldAngle = $(imgs[i]).getAngle(),
+              newAngle = $(imgs[ind]).getAngle();
+          if (clockwise) {
+            if (oldAngle > newAngle)
+              newAngle += 360;
+          } else {
+            if (oldAngle < newAngle)
+              newAngle -= 360;
+          }
+          $(imgs[i]).memoNext(newAngle);
+        }
+        imgs.addClass('animate').removeClass('active');
+        $(this).addClass('active');
+        imgs.setNext($($container).data('opt').radius);
+        imgs.trigger('mouseleave');
+        setTimeout(function() {
+          imgs.trigger('mouseleave');
+          imgs.removeClass('animate')} , 1500); // this fixes bug after veel animation
+      });
+
+      imgs.mouseenter(function(evt) {
+        if ($('body.mobile').length > 0 || $(this).hasClass('animate')) {
+          return;
+        }
+        var $this = $(this);
+        var $image = $('.wheel-item-image', $this);
+        var $function = $('.function', $this);
+        $image.addClass('hover');
+        if ($this.hasClass('active')) {
+          return;
+        }
+        $this.mousemove(function(evt) {
+          var thisOffset = $this.offset(),
+              mouseOffset = evt.pageX - thisOffset.left,
+              thisCenter = $this.width() / 2,
+              angle = (mouseOffset - thisCenter)*0.6;
+              angle = angle > 45 ? 45 : angle;
+              angle = angle < -45 ? -45 : angle;
+          var transform = 'rotate3d(-0.1, 1, -0.1, {angle}deg) translate3d(0, 0, {z}px)'.replace('{angle}', angle);
+          $image.css({
+            'transform': transform.replace('{z}', '-5'),
+            'transition': 'none'
+          });
+          $function.css({
+            'transform': transform.replace('{z}', '20'),
+            'transition': 'none'
+          });
+        });
+      });
+      imgs.mouseleave(function() {
+        $('.wheel-item-image, .function', $(this)).removeClass('hover').removeAttr('style');
+        $(this).off('mousemove');
+      });
+
+    });
+  }
+}(jQuery));
