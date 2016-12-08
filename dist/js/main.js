68b5f46e372cf828caacf02bb87e2c16ab4cdf99
Eugene <e.lavrinenkov@gmail.com>
2016-12-08 16:23:56 +0200

    Add animations to sections

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,72 @@
 $(document).ready(function () {
-  $('#fullpage').fullpage({
-    css3: true,
-    navigation: true,
-    navigationTooltips: ['home', 
-                         'intro', 
-                         'vi', 
-                         'advertisers', 
-                         'publishers', 
-                         'servicelevels', 
-                         'highlight',
-                         'team',
-                         'news',
-                         'events',
-                         'contact']
-  });
 
-  $('.meet-the-team').veel({});
+  var startDesktop = function () {
+
+    $('#advertiser_veel').veel({
+      radius: 200,
+      activeAngle: 0,
+      startAngle: 115,
+      endAngle: 130
+    });
+
+    $('#publishers_veel').veel({
+      radius: 200,
+      startAngle: 295,
+      endAngle: 130
+    });
+
+    $('#team_veel').veel({});
+
+    $('#fullpage').fullpage({
+      css3: true,
+      navigation: true,
+      keyboardScrolling: true,
+      responsiveWidth: 768,
+      navigationTooltips: ['home', 
+                          'intro', 
+                          'vi', 
+                          'advertisers', 
+                          'publishers', 
+                          'servicelevels', 
+                          'highlight',
+                          'team',
+                          'news',
+                          'events',
+                          'contact']
+    });
+
+  }
+
+  var isDesktopWidth = function () {
+
+    if (document.body.clientWidth >= 768) {
+      return true;
+    } else {
+      return false;
+    }
+
+  };
+
+  if (isDesktopWidth()) {
+
+    startDesktop();
+
+  }
+
+  window.addEventListener('resize', function () {
+
+    if (!isDesktopWidth()) {
+
+      console.log('bingo');
+      // $.fn.fullpage.setMouseWheelScrolling(false);
+      $.fn.fullpage.setAllowScrolling(false);
+
+    } else {
+
+      startDesktop();
+
+    }
+    
+  });
 
 });
