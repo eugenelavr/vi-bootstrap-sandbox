d1421f186eec5645a1e8a03031a4becae98e66f2
Eugene <e.lavrinenkov@gmail.com>
2016-12-15 18:00:51 +0200

    minor fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,42 +1,27 @@
 $(document).ready(function () {
 
-  var startDesktop = function () {
+  var startDesktop = function (settings) {
 
-    $('.wrapper').removeClass('hidden');    
+    if (settings.veel) {
+      $('.wrapper').removeClass('hidden');    
 
-    $('#advertiser_veel').veel({
-      radius: 200,
-      activeAngle: 0,
-      startAngle: 115,
-      endAngle: 130
-    });
+      $('#advertiser_veel').veel({
+        radius: 200,
+        activeAngle: 0,
+        startAngle: 115,
+        endAngle: 130
+      });
 
-    $('#publishers_veel').veel({
-      radius: 200,
-      startAngle: 295,
-      endAngle: 130
-    });
+      $('#publishers_veel').veel({
+        radius: 200,
+        startAngle: 295,
+        endAngle: 130
+      });
 
-    $('#team_veel').veel({});
-
-  };
-
-  var isDesktopWidth = function () {
-
-    if (document.body.clientWidth >= 768) {
-      return true;
-    } else {
-      return false;
+      $('#team_veel').veel({});
     }
 
-  };
-
-  if (isDesktopWidth()) {
-
-    if (window.location.href.includes('#')) {
-
-      startDesktop();       
-
+    if (settings.fullpage) {
       $('#fullpage').fullpage({
         css3: true,
         navigation: true,
@@ -57,6 +42,28 @@ $(document).ready(function () {
         ],
         sectionsColor: ['', '', '', '', '', '', '', '', '', '', '#000']
       });
+    }
+
+  };
+
+  var isDesktopWidth = function () {
+
+    if (document.body.clientWidth >= 768) {
+      return true;
+    } else {
+      return false;
+    }
+
+  };
+
+  if (isDesktopWidth()) {
+
+    if (window.location.href.includes('#')) {
+
+      startDesktop({
+        veel: true,
+        fullpage: true
+      });
 
     } else {
 
@@ -67,30 +74,12 @@ $(document).ready(function () {
           $(this).remove();
         });
 
-        startDesktop();        
-
-        $('#fullpage').fullpage({
-          css3: true,
-          navigation: true,
-          keyboardScrolling: true,
-          normalScrollElements: '.modal',
-          navigationTooltips: [
-            'home', 
-            'intro', 
-            'vi', 
-            'advertisers', 
-            'publishers', 
-            'servicelevels', 
-            'highlight',
-            'team',
-            'news',
-            'events',
-            'contact'
-          ],
-          sectionsColor: ['', '', '', '', '', '', '', '', '', '', '#000']
+        startDesktop({
+          veel: true,
+          fullpage: true
         });
 
-    });
+      });
 
     }
 
@@ -100,27 +89,17 @@ $(document).ready(function () {
 
   window.addEventListener('resize', function () {
 
-    // if (!isDesktopWidth() && $.fn.fullpage.setAllowScrolling) {
-
-    //   console.log('bingo');
-    //   // $.fn.fullpage.setMouseWheelScrolling(false);
-    //   $.fn.fullpage.setAllowScrolling(false);
-
-    // } else {
-
-    //   startDesktop();
-
-    // }
-
     if (isDesktopWidth) {
-      startDesktop();
+      startDesktop({
+        veel: true
+      });
     }
     
   });
 
-    $('section.news').fillNewsEvents('./dist/news/news-content.html', 'news');
-    $('section.events').fillNewsEvents('./dist/events/events-content.html', 'events');
-    $('#privacyPolicy .modal-body').fillPrivacyTerms('./dist/privacy-policy.html');
-    $('#terms .modal-body').fillPrivacyTerms('./dist/terms.html');
+  $('section.news').fillNewsEvents('./dist/news/news-content.html', 'news');
+  $('section.events').fillNewsEvents('./dist/events/events-content.html', 'events');
+  $('#privacyPolicy .modal-body').fillPrivacyTerms('./dist/privacy-policy.html');
+  $('#terms .modal-body').fillPrivacyTerms('./dist/terms.html');
 
 });
